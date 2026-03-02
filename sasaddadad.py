3bb284fd0da34c02a2dc718c0c25deead819175a
Your Name <you@example.com>
2026-03-02 12:22:28 +0900

    이상한 출력기능 만듬

--- sasaddadad.py
+++ sasaddadad.py
@@ -1,3 +1,5 @@
 import pandas
 
-pandas.read_csv("C:\\Users\\우리가족\\Downloads\\microbit.csv")
+log = pandas.read_csv("C:\\Users\\우리가족\\Downloads\\microbit.csv")
+for i in range(len(log)) :
+    print(log.iloc[0])
