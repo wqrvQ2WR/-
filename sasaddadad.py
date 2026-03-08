29274091789b67c665c6e82033f6ad061eae78d9
Your Name <you@example.com>
2026-03-08 16:39:52 +0900

    다른 이상한 타자 프로그램 만듬

--- sasaddadad.py
+++ sasaddadad.py
@@ -1,23 +1,15 @@
-import pandas
-import sys
+import time
 
-list1 = []
-log = pandas.read_csv("C:\\Users\\우리가족\\Downloads\\microbit.csv")
-for i in range(len(log)) :
-    print(log.iloc[i])
-    list1.append(log.iloc[i])
-user = ""
+StartTime = time.time()
+입력 = input("문자열 입력: ")
+EndTime = time.time()
 
-repeat = True
-while repeat :
-    try :
-        user = input("몇번째 데이터를 원하냐 닝겐: ")
-        userint = int(user)
-        print(list1[userint])
-    except ValueError :
-        if user == 'q' :
-            sys.exit()
-        else :
-            pass
-    except IndexError :
-        pass
+걸린시간_초 = EndTime - StartTime
+걸린시간_분 = 걸린시간_초 / 60
+
+단어수 = len(입력) / 5   # 1단어 = 5글자 기준
+WPM = 단어수 / 걸린시간_분
+
+print("걸린 시간:", 걸린시간_초, "초")
+print("추정 단어 수:", 단어수)
+print("타자 속도:", WPM, "WPM")
