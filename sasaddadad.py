b7636173681f051b1d8d0dd9e7f5d430447544c0
Your Name <you@example.com>
2026-03-28 16:55:26 +0900

    와쩐당

--- sasaddadad.py
+++ sasaddadad.py
@@ -1,3 +1,13 @@
-from faker import Faker
+import turtle
+import random
 
-print(Faker.address())
+turtle.hideturtle()
+turtle.speed(10)
+
+colors = ["red", "blue", "green", "purple", "orange", "black"]
+
+while True :
+    turtle.color(random.choice(colors))
+    x = random.randint(-300,300)
+    y = random.randint(-300, 300)
+    turtle.goto(x, y)
